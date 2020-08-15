54fa4bb40f8ded50dcb8380220b8911cfe4f8bb0
nomi474 <nomi474@yahoo.com>
2020-08-15 16:41:51 +0400

    pushing code to repo

---
+++ bootjpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'nomi','Java');
+insert into alien values(102,'amina','Python');
+insert into alien values(103,'rayhan','Javascript');
+insert into alien values(104,'alisha','C#');
+insert into alien values(105,'aafiya','php');
---
+++ springdatarest/src/main/java/com/teresco/demo/dao/AlienRepo.java
@@ -0,0 +1,11 @@
+package com.teresco.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.teresco.demo.model.Alien;
+
+@RepositoryRestResource(collectionResourceRel="aliens", path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+}
